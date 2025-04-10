d0b76b8ec3f5085ab2e8cf4ec014ce0d87afbcd3
Santiago Mesa <mesasantiago5@gmail.com>
2025-04-10 15:53:57 -0500

    intento de java fx

--- src/main/java/com/javeriana/sistema/ui/MainApp.java
+++ src/main/java/com/javeriana/sistema/ui/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/com/javeriana/sistema/ui/LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Sistema Bancario - Login");
         stage.setScene(scene);
