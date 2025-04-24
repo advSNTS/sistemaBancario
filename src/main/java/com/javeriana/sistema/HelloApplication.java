66df02664402de6945f3bdd557b3f1a4e7a2324f
Santiago Mesa <mesasantiago5@gmail.com>
2025-04-24 00:30:51 -0500

    integracion luis santiago

--- src/main/java/com/javeriana/sistema/HelloApplication.java
+++ src/main/java/com/javeriana/sistema/HelloApplication.java
@@ -5,14 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javeriana/sistema/ui/WelcomeView.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Sistema Bancario");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,12 @@ module com.javeriana.sistema {
     requires javafx.fxml;
     requires java.sql;
 
-    opens com.javeriana.sistema.ui to javafx.fxml;
-    exports com.javeriana.sistema.ui;
-
     opens com.javeriana.sistema.controller to javafx.fxml;
-    exports com.javeriana.sistema.controller;
+    opens com.javeriana.sistema.dao to javafx.fxml;
+    opens com.javeriana.sistema.model to javafx.fxml;
+    opens com.javeriana.sistema.services to javafx.fxml;
 
-    opens com.javeriana.sistema.dao to java.sql;
-    exports com.javeriana.sistema.dao;
+    exports com.javeriana.sistema;
+    exports com.javeriana.sistema.controller;
+    exports com.javeriana.sistema.model;
 }
