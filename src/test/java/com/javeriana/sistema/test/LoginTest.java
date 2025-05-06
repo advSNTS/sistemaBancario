caa365fb313156c33abc740c8e5f58871489d737
advSNTS <187468015+advSNTS@users.noreply.github.com>
2025-05-06 11:47:31 -0500

    Login test pulido

--- src/test/java/com/javeriana/sistema/test/LoginTest.java
+++ src/test/java/com/javeriana/sistema/test/LoginTest.java
@@ -5,15 +5,10 @@ import com.javeriana.sistema.services.UsuarioService;
 import com.javeriana.sistema.util.DBConnection;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.sql.Connection;
-import java.sql.Statement;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
     private UsuarioService usuarioService;
